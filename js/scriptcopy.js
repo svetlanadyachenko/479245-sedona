51df24ff16a8dc14d817aae2b3ddfe0558482015
svetlanadyachenko <angelinalana84@gmail.com>
2018-12-23 20:42:52 +0300

    Внесение правок

---
+++ js/scriptcopy.js
@@ -0,0 +1,39 @@
+var link = document.querySelector(".search-in-sedona");
+var popup = document.querySelector(".search-form");
+var datein = popup.querySelector("[name=datein]");
+var form = popup.querySelector("form");
+var dateout = popup.querySelector("[name=dateout]");
+var adults = popup.querySelector("[name=adults]");
+var kids = popup.querySelector("[name=kids]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("search-form-show");
+  datein.focus();
+});
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+if (popup.classList.contains("search-form-show")) {
+  popup.classList.remove("search-error");
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!datein.value || !dateout.value || !adults.value || !kids.value) {
+    evt.preventDefault();
+    popup.classList.remove("search-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("search-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+     evt.preventDefault();
+     if (popup.classList.contains("search-form-show")) {
+       popup.classList.remove("search-form-show");
+       popup.classList.remove("search-error");
+     }
+   }
+ });
